f6ae7edfcd8d345f0638cb25c2a87278aca63171
Nathan Dixon <nathan.dixon@evadon.com>
2025-02-18 18:57:02 +0000

    PPRD hello world

--- appOld.vue
+++ appOld.vue
@@ -1,15 +1,6 @@
-<script setup lang="ts">
-const version = 2 + 1;
-</script>
-
 <template>
-  <div class="hello">Hello Nuxt {{ version }}!</div>
+  <div>
+    <NuxtRouteAnnouncer />
+    <NuxtWelcome />
+  </div>
 </template>
-
-<style scoped>
-.hello {
-  font-family: Arial, Helvetica, sans-serif;
-  font-size: 3rem;
-  padding: 2rem;
-}
-</style>
